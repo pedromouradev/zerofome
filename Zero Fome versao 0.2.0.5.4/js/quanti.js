e7186999de2bb03f8ef4585445e094a367dc3057
Pedro Moura <contatopedromoura07@gmail.com>
2025-10-09 07:47:03 -0300

    Delete Zero Fome versao 0.2.0.5.4 directory

--- Zero Fome versao 0.2.0.5.4/js/quanti.js
+++
@@ -1,8 +0,0 @@
-function mostrarDoacao() {
-    var valor = document.getElementById("dinheiro").value;
-    if (valor.trim() === "") {
-        alert("Por favor, digite um valor para doar.");
-    } else {
-        alert("Valor doado com sucesso. R$ " + valor);
-    }
-}
